34086eece62fec4209732a8c3360ca2129750e79
fullautocode <rusakpeter9191@outlook.com>
2024-04-15 23:26:59 -0700

    some updates

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from config import Config
 
 myappFlaskInstance = Flask(__name__)
+myappFlaskInstance.config.from_object(Config)
 
-from myapp import routes 
+from myapp import routes

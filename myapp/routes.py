7a7d3175fbfeb7cf4ef636fdd39117359a643057
fullautocode <rusakpeter9191@outlook.com>
2024-04-16 12:37:12 -0700

    /login

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from myapp import myappFlaskInstance
+from myapp.forms import LoginForm
 
 @myappFlaskInstance.route('/')
 @myappFlaskInstance.route('/index')
@@ -16,3 +17,8 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@myappFlaskInstance.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)

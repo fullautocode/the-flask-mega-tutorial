78cb4aaa25e65f7b614d206ecc2f60f8279c9d03
fullautocode <rusakpeter9191@outlook.com>
2024-04-16 21:54:37 -0700

    web forms completed

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Flaskform
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired 
 

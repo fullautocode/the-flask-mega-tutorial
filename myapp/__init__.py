19837433fabb364f3efd1d120e2f7af326d935d2
fullautocode <rusakpeter9191@outlook.com>
2024-04-17 23:31:30 -0700

    db in work in progess, halfway there

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 myappFlaskInstance = Flask(__name__)
 myappFlaskInstance.config.from_object(Config)
+db = SQLAlchemy(myappFlaskInstance)
+migrate = Migrate(myappFlaskInstance, db)
 
-from myapp import routes
+from myapp import routes, models 

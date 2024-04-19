1042e6eecd3328755780812dbbebe4920a5f5878
fullautocode <rusakpeter9191@outlook.com>
2024-04-18 21:37:30 -0700

    updated blog.py
    added app.shell_context_processor decorate to register the function as a shell context function

--- blog.py
+++ blog.py
@@ -1,1 +1,8 @@
-from myapp import myappFlaskInstance
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from myapp import myappFlaskInstance, db
+from myapp.models import User, Post
+
+@myappFlaskInstance.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}

ba8f35c5b073fa485eca8c871686e9a98d826d95
fullautocode <rusakpeter9191@outlook.com>
2024-04-20 21:34:24 -0700

    Added usermixin and hash function for hash_password

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 myappFlaskInstance = Flask(__name__)
 myappFlaskInstance.config.from_object(Config)
 db = SQLAlchemy(myappFlaskInstance)
 migrate = Migrate(myappFlaskInstance, db)
+login = LoginManager(myapp)
 
 from myapp import routes, models 
